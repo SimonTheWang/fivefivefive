51f629efab4f689180a2e73a307bcc1d4f7e1df0
SimonTheWang <simonwg2001@gmail.com>
2025-01-24 20:10:43 -0500

    init commit

---
+++ fivefivefive/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>coming soon <span className="animate-pulse delay-0">.</span><span className="animate-pulse delay-300">.</span><span className="animate-pulse delay-600">.</span></h1>
+      <p className="text-2xl text-center pt-10 w-full">- fivefivefive</p>
+    </>
+  )
+}
+
+export default App
